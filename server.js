37dac32a13acc0d1fd384525f317294d22646b50
PDimitroww <plamen.vn.96@gmail.com>
2023-04-04 21:21:36 +0300

    initial commit

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.listen(5000, () => console.log("Server Running"));
 const contactEmail = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "plamen.vn.96@gmail.com",
-    pass: "tlqtegedoizgvjhj",
+    user: "*************@gmail.com",
+    pass: "***********",
   },
 });
 
